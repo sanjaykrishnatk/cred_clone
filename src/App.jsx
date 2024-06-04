889e0fbfd8e646e94c97b73ec0c0b1c6f4d8a887
SANJAY <sktk9909@gmail.com>
2024-06-04 21:02:16 +0530

    First page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import Home from "./Home";
+import Menu from "./Menu";
+import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Home.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import videoBg from "./assets/hero-desktop.mp4";
+import mobileBg from "./assets/hero-mobile.mp4";
+import Menu from "./Menu";
+import "./Home.css";
+
+function Home() {
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const setMobile = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    setMobile(); // Set initial state
+    window.addEventListener("resize", setMobile); // Update state on resize
+    return () => window.removeEventListener("resize", setMobile);
+  }, []);
+
+  return (
+    <div className="main">
+      {!isMobile && (
+        <video className="bgVideo w-100" muted autoPlay loop>
+          <source src={videoBg} />
+        </video>
+      )}
+      {isMobile && (
+        <video className="mobileVideo w-100" muted autoPlay loop>
+          <source src={mobileBg} />
+        </video>
+      )}
+      <div className="menu-section">
+        <Menu />
+      </div>
+      <div className="content bg-transparent text-center">
+        <h1 className="bg-transparent text-center">
+          crafted for the
+          <br /> creditworthy
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
